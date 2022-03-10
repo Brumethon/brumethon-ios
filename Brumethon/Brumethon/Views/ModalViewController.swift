adc552db97ce5afd15a8eae786b1a60c816e0b3b
Nixo <tran.nicolas28@gmail.com>
2022-03-11 00:35:46 +0100

    Carousel ready

--- Brumethon/Brumethon/Views/ModalViewController.swift
+++ Brumethon/Brumethon/Views/ModalViewController.swift
@@ -6,23 +6,13 @@
 //
 
 import UIKit
-import DropDown
 
 class ModalViewController: UIViewController {
 
-    @IBOutlet weak var categoriesButton: UIButton!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var positionSegmentControl: UISegmentedControl!
     @IBOutlet weak var askHelpButton: UIButton!
         
-    //Init dropdown
-    let categoriesDropDown = DropDown()
-    
-    lazy var dropDowns: [DropDown] = {
-            return [
-                self.categoriesDropDown
-            ]
-        }()
     
     let positionsOptions : [String] = [
         languageUtil.getTranslatedText(translationString: "home_address"),
@@ -31,13 +21,6 @@ class ModalViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Stylish that shitty button (a lil bit)
-        self.categoriesButton.layer.cornerRadius = 10
-        self.categoriesButton.setTitle(languageUtil.getTranslatedText(translationString: "categories"), for: .normal)
-        
-        customizeDropDown()
-        setupCategoriesDropDown()
         
         //Stylish that text view
         self.descriptionTextView.layer.cornerRadius = 10
@@ -51,53 +34,6 @@ class ModalViewController: UIViewController {
             self.positionSegmentControl.setTitle(positionsOptions[index], forSegmentAt: index)
         }
        
-    }
-    
-    
-    //-------------------- CONCERNING DROP DOWN --------------------//
-    
-    @IBAction func handleCategories(_ sender: Any) {
-        categoriesDropDown.show()
-    }
-   
-    func setupCategoriesDropDown(){
-        categoriesDropDown.anchorView = categoriesButton
-        
-        //Display the list under the button
-        categoriesDropDown.bottomOffset = CGPoint(x: 0, y: categoriesButton.bounds.height + 5)
-        
-        categoriesDropDown.dataSource = [
-            "Carosserie",
-            "Mécanique",
-            "Questions"
-        ]
-        
-        categoriesDropDown.selectionAction = { [weak self] (index, item) in
-                    self?.categoriesButton.setTitle(item, for: .normal)
-                }
-        
-        categoriesDropDown.multiSelectionAction = { [weak self] (indices, items) in
-            if items.isEmpty {
-                self?.categoriesButton.setTitle(languageUtil.getTranslatedText(translationString: "categories"), for: .normal)
-            }
-        }
-    }
-    
-    func customizeDropDown() {
-        
-            let appearance = DropDown.appearance()
-            
-            appearance.cellHeight = 60
-            appearance.backgroundColor = UIColor(white: 1, alpha: 1)
-            appearance.selectionBackgroundColor = UIColor(red: 0.6494, green: 0.8155, blue: 1.0, alpha: 0.2)
-            appearance.separatorColor = UIColor(white: 0.7, alpha: 0.8)
-            appearance.cornerRadius = 10
-            appearance.shadowColor = UIColor(white: 0.6, alpha: 1)
-            appearance.shadowOpacity = 0.9
-            appearance.shadowRadius = 25
-            appearance.animationduration = 0.25
-            appearance.textColor = .darkGray
-            
     }
     
     //-------------------- CONCERNING BUTTON --------------------//
--- Brumethon/Brumethon/Views/NavigationViews/MainViewController.swift
+++ Brumethon/Brumethon/Views/NavigationViews/MainViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 import CoreLocation
+import iCarousel
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, iCarouselDataSource {
     
     var locationManager : CLLocationManager!
     
+    @IBOutlet weak var containerView: UIView!
+    
     //Buttons[]
     @IBOutlet weak var askHelpButton: UIButton!
+    
+    let myCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .rotary
+        return view
+    }()
     
 
     override func viewDidLoad() {
@@ -21,27 +30,41 @@ class MainViewController: UIViewController {
 
         self.locationManager = CLLocationManager()
         
-        
-        
         askUserAuthorizationToLocate(locationManager: self.locationManager)
         
         self.askHelpButton.setTitle(languageUtil.getTranslatedText(translationString: "main.ask_for_help"), for: .normal)
+        
+        containerView.addSubview(myCarousel)
+        
+        self.myCarousel.dataSource = self
+        self.myCarousel.frame      = CGRect(x: 0, y: 0, width: containerView.frame.size.width, height: containerView.frame.size.height)
         
     }
     
     @IBAction func handleHelp(_ sender: Any) {
         self.present(ModalViewController(), animated: true)
     }
-                                                                   
-    @objc
-    func handleProfile(){
-        print("test")
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 10
     }
     
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.containerView.frame.width, height: self.containerView.frame.height))
+        
+        view.layer.cornerRadius = 10
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = UIColor.black.cgColor
+        view.backgroundColor = .white
+        
+        
+        return view
+    }
+                                                                   
+    
 }
+
 
 func askUserAuthorizationToLocate(locationManager : CLLocationManager) {
     locationManager.requestWhenInUseAuthorization()
 }
-
-
