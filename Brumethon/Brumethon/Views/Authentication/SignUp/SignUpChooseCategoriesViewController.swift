b6dbc44685474b37bf9aab1d66e328d5cc541223
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2022-03-10 20:46:45 +0100

    WIP signup categories

---
+++ Brumethon/Brumethon/Views/Authentication/SignUp/SignUpChooseCategoriesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpChooseCategoriesViewController.swift
+//  Brumethon
+//
+//  Created by Gwendal on 10/03/2022.
+//
+
+import UIKit
+
+class SignUpChooseCategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
